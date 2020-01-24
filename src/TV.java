f682f80539d69fca7f717cbeda434d5db64097f6
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-01-24 10:47:43 +0100

    Tworzenie klas i obiektów z Twojego otoczenia 1.2

--- src/TV.java
+++ src/TV.java
@@ -1,20 +1,10 @@
 public class TV {
         public static void main(String[] args) {
 
-                TvDisplay display1 = new TvDisplay();
-                display1.color = "black";
-                display1.material = "amelinium";
-                display1.matrix = "IPS";
+                TvDisplay display1 = new TvDisplay("black", "aluminum", "IPS");
+                display1.showInfo();
 
-                String display1Info = "Color: " + display1.color + " material: " + display1.material + " matrix: " + display1.matrix;
-                System.out.println(display1Info);
-
-                TvDisplay display2 = new TvDisplay();
-                display2.color = "white";
-                display2.material = "plastics ";
-                display2.matrix = "AV";
-
-                String display2Info = "Color: " + display2.color + " material: " + display2.material + "matrix: " + display2.matrix;
-                System.out.println(display2Info);
+                TvDisplay display2 = new TvDisplay("white", "plastics", "AV");
+                display2.showInfo();
         }
 }
--- src/TvDisplay.java
+++ src/TvDisplay.java
@@ -3,4 +3,14 @@ public class TvDisplay {
     String matrix;
     String material;
 
+    TvDisplay(String col, String mat, String mate) {
+        color = col;
+        matrix = mat;
+        material = mate;
+
+    }
+
+    void showInfo() {
+        System.out.println("Color: " + color + " material: " + material + " matrix: " + matrix);
+    }
 }
